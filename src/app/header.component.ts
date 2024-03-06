f58173270f1182af2b940f1f6ab0d27fa2f8ef46
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-03-06 17:53:49 +0530

    feat: header

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,11 +1,60 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
-
+import { MatToolbarModule } from "@angular/material/toolbar";
+// import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [],
-  template: ` <p>header</p> `,
-  styles: ``,
+  imports: [MatToolbarModule, MatButtonModule],
+  template: `
+    <p>
+      <mat-toolbar color="primary">
+        <span style="margin-right: 10px;">InventoryMgt</span>
+        <button mat-button routerLink="/home" routerLinkActive="active">
+          Home
+        </button>
+        <button mat-button routerLink="" routerLinkActive="active">
+          Category
+        </button>
+        <button mat-button routerLink="" routerLinkActive="active">
+          Product
+        </button>
+        <span class="example-spacer"></span>
+
+        <button mat-button>Login</button>
+
+        <a
+          href="https://github.com/rd003/"
+          target="_blank"
+          mat-icon-button
+          class="example-icon favorite-icon"
+          aria-label="Example icon-button with heart icon"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+            />
+          </svg>
+        </a>
+      </mat-toolbar>
+    </p>
+  `,
+  styles: [
+    `
+      .example-spacer {
+        flex: 1 1 auto;
+      }
+
+      .active {
+        background: #003a85;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {}
