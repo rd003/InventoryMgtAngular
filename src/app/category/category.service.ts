7d0cfe1c257e60ea821274dc23f57e80af0fdcf5
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-03-07 12:09:37 +0530

    feat: category service

--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -11,6 +11,24 @@ export class CategoryService {
   private apiUrl = environment.API_BASE_URL + "/categories";
   private http = inject(HttpClient);
 
+  addCategory(category: CategoryModel): Observable<CategoryModel> {
+    return this.http.post<CategoryModel>(this.apiUrl, category);
+  }
+
+  updateCategory(category: CategoryModel): Observable<any> {
+    const url = `${this.apiUrl}/${category.id}`;
+    return this.http.put<any>(url, category);
+  }
+
+  deleteCategory(id: number): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete(url);
+  }
+  getCategory(id: number): Observable<CategoryModel> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<CategoryModel>(url);
+  }
+
   getCategories(): Observable<CategoryModel[]> {
     return this.http.get<CategoryModel[]>(this.apiUrl);
   }
