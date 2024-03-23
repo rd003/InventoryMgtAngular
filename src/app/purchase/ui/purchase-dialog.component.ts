7ebf60ffb5c9023e336a730e934669b2b13c00e3
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-03-23 21:21:45 +0530

    feat: purchase dialog component created

---
+++ src/app/purchase/ui/purchase-dialog.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+  selector: "app-purchase-dialog",
+  standalone: true,
+  imports: [],
+  template: ``,
+  styles: [``],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PurchaseDialogComponent {}
